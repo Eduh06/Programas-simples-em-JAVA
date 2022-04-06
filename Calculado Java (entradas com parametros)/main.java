7b1e517c4ae16ad9b6f7e01ed7991c13550359f2
Eduardo dos Santos <37030387+Eduh06@users.noreply.github.com>
2022-04-06 20:02:04 -0300

    Update main.java

--- Calculado Java (entradas com parametros)/main.java
+++ Calculado Java (entradas com parametros)/main.java
@@ -7,7 +7,9 @@ public class main {
 		System.out.println("Exercicio Calculadora");
 		
 		Calculadora.Soma(9,8);
-		Calculadora.Subtração(9, 3);
+		Calculadora.SubtraÃ§Ã£o(9, 3);
+		Calculadora.divisao(9, 3);
+		Calculadora.multiplicacao(3,9);
 	}
 
 }
