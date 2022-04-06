14c0a3659381e08a2b2d1d55886b495225ed1621
Eduardo dos Santos <37030387+Eduh06@users.noreply.github.com>
2022-04-06 20:01:27 -0300

    Calculadora
    Programa Recebe os parâmetro e retorna os resultados de multiplicação, divisão, soma e subtração.

---
+++ Calculado Java (entradas com parametros)/Calculadora.java
@@ -0,0 +1,31 @@
+
+public class Calculadora {
+
+	public static void Soma(double  numero1, double numero2) {
+		double resultado = numero1 + numero2;
+		System.out.println("A soma de " + numero1 + " + " + numero2 + " = " + resultado);
+	}
+
+	public static void Subtração(double  numero1, double numero2) {
+		double resultado = numero1 - numero2;
+		System.out.println(" A subtração de " + numero1 + " - " + numero2 + " = " + resultado);
+	}
+	
+	
+	public static void multiplicacao(double  numero1, double numero2) {
+		double resultado = numero1 * numero2;
+		System.out.println(" A multiplicação de " + numero1 + " x " + numero2 + " = " + resultado);
+	}
+	
+	public static void divisao(double  numero1, double numero2) {
+		double resultado = numero1 / numero2;
+		System.out.println("A divisão de " + numero1 + " / " + numero2 + " = " + resultado);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Calculado Java (entradas com parametros)/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		//calculadora
+		System.out.println("Exercicio Calculadora");
+		
+		Calculadora.Soma(9,8);
+		Calculadora.Subtração(9, 3);
+	}
+
+}
