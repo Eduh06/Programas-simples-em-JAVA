4ac7d872a3ebaaffc267f5e309156868562552a5
Eduardo dos Santos <37030387+Eduh06@users.noreply.github.com>
2022-04-06 22:22:25 -0300

    Add files via upload
    Programa para calcular área do quadrado, retangulo e trapézio.

---
+++ calculoArea(Trapezio,Quadrado, Retangulo)/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Calculo de área");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5,5);
+		Quadrilatero.area(2,2,2);
+
+	}
+
+}
+	
